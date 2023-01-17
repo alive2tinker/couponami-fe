9b7ca03bfaa6f2712a17a5feaa6676f7370de576
Abdulmalik Alsufayran <alsufiran@gmail.com>
2023-01-17 16:08:43 +0300

    firebase updatres

--- src/main.js
+++ src/main.js
@@ -1,39 +1,64 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+import "@ionic/vue/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+import "@ionic/vue/css/normalize.css";
+import "@ionic/vue/css/structure.css";
+import "@ionic/vue/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+import "@ionic/vue/css/padding.css";
+import "@ionic/vue/css/float-elements.css";
+import "@ionic/vue/css/text-alignment.css";
+import "@ionic/vue/css/text-transformation.css";
+import "@ionic/vue/css/flex-utils.css";
+import "@ionic/vue/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-import './assets/css/app.css';
+import "./assets/css/app.css";
 
-import store from './store'
-import i18n from './i18n'
+import store from "./store";
+import i18n from "./i18n";
 
-const app = createApp(App).use(store).use(i18n).use(store)
-  .use(IonicVue,{
-    mode:'ios'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlFAzEh7xMxQYYBCJAWVMFNLxD5w_XVHs",
+  authDomain: "couponami-12844.firebaseapp.com",
+  projectId: "couponami-12844",
+  storageBucket: "couponami-12844.appspot.com",
+  messagingSenderId: "283845945415",
+  appId: "1:283845945415:web:92c8ab6a5aacc3f72ba962",
+  measurementId: "G-46CLGFY86D"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+getAnalytics(firebaseApp);
+
+const app = createApp(App)
+  .use(store)
+  .use(i18n)
+  .use(store)
+  .use(IonicVue, {
+    mode: "ios",
   })
   .use(router);
 
 router.isReady().then(() => {
-  app.mount('#app');
+  app.mount("#app");
 });
--- src/views/Auth/RegisterPage.vue
+++ src/views/Auth/RegisterPage.vue
@@ -13,36 +13,60 @@
                     </ion-buttons>
                 </ion-toolbar>
             </ion-header>
-            <div class="max-w-7xl mx-auto p-4">
+            <!-- registration form -->
+            <div class="max-w-7xl mx-auto p-4" v-show="!smsSent">
                 <ion-text>
                     <h1 class="font-bold text-3xl">{{ $t("Let's Register you in") }}</h1>
                 </ion-text>
                 <ion-text>
-                    <p class="py-4 text-2xl font-thin">{{  $t('Welcome to your Couponami!') }}</p>
+                    <p class="py-4 text-2xl font-thin">{{ $t('Welcome to your Couponami!') }}</p>
                 </ion-text>
                 <div class="flex h-72">
                     <div class="m-auto flex-1 space-y-3">
-                        <ion-input class="border border-zinc-200 rounded-xl indent-2"
-                            :placeholder="$t('username')" v-model="form.username"></ion-input>
-                            <ion-input class="border border-zinc-200 rounded-xl indent-2"
-                            :placeholder="$t('phone number')" v-model="form.phone"></ion-input>
+                        <ion-input class="border border-zinc-200 rounded-xl indent-2" :placeholder="$t('username')"
+                            v-model="form.username"></ion-input>
+                        <ion-input class="border border-zinc-200 rounded-xl indent-2" :placeholder="$t('phone number')"
+                            v-model="form.phone"></ion-input>
                         <ion-input type="password" class="border border-zinc-200 rounded-xl indent-2"
                             :placeholder="$t('Password')" v-model="form.password"></ion-input>
-                            <ion-input type="password" class="border border-zinc-200 rounded-xl indent-2"
+                        <ion-input type="password" class="border border-zinc-200 rounded-xl indent-2"
                             :placeholder="$t('Password Confirmation')" v-model="form.passwordConfirmation"></ion-input>
                     </div>
                 </div>
                 <div class="flex justify-between">
                     <ion-text>
-                        <p class="font-medium subpixel-antialiased">{{ $t('have an account already?')}}</p>
+                        <p class="font-medium subpixel-antialiased">{{ $t('have an account already?') }}</p>
                     </ion-text>
-                    <ion-button fill="clear" class="-mt-3" id="registerButton" @click="router.push({name: 'Login'})">{{ $t('Login')}}</ion-button>
+                    <ion-button fill="clear" class="-mt-3" id="registerButton"
+                        @click="router.push({ name: 'Login' })">{{
+                            $t('Login')
+                        }}</ion-button>
                 </div>
             </div>
+            <!-- otp confirmation -->
+            <div class="max-w-7xl mx-auto p-4" v-show="smsSent">
+                <ion-text>
+                    <h1 class="font-bold text-3xl">{{ $t("Enter the code") }}</h1>
+                </ion-text>
+                <ion-text>
+                    <p class="text-sm text-zinc-600">{{ $t('Enter the four-digit code that was sent to you at {phone}', {phone:this.form.phone})}}</p>
+                </ion-text>
+                <v-otp-input ref="otpInput" input-classes="otp-input" separator="-" :num-inputs="4"
+                    :should-auto-focus="true" :is-input-num="true" :conditionalClass="['one', 'two', 'three', 'four']"
+                    @on-change="handleOnChange" @on-complete="handleOnComplete" />
+            </div>
+            <div id="sign-in-button" style="display:none"></div>
         </ion-content>
         <ion-footer :translucent="true" class="ion-no-border ion-padding">
-                <ion-button color="primary" expand="block" :disabled="!registrationDisabled">{{ $t('Register')}}</ion-button>
-            </ion-footer>
+            <ion-button color="primary" expand="block" :disabled="!registrationDisabled" @click="signIn"
+                v-show="!smsSent">{{
+                    $t('Register')
+                }}</ion-button>
+            <ion-button color="primary" expand="block" :disabled="!registrationDisabled" @click="confirmOTP"
+                v-show="smsSent">{{
+    $t('Verify OTP')
+                }}</ion-button>
+        </ion-footer>
     </ion-page>
 </template>
 <script >
@@ -50,35 +74,76 @@ import { IonInput, IonFooter, IonButton, IonText, IonToolbar, IonTitle, IonPage,
 import { defineComponent } from 'vue';
 import { mapActions, mapGetters } from 'vuex';
 import { useRouter } from 'vue-router';
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
+import VOtpInput from 'vue3-otp-input';
+
 
 export default defineComponent({
-    components:{
-        IonInput, IonFooter, IonButton, IonText, IonToolbar, IonTitle, IonPage, IonHeader, IonContent, IonButtons, IonBackButton
+    components: {
+        IonInput, VOtpInput, IonFooter, IonButton, IonText, IonToolbar, IonTitle, IonPage, IonHeader, IonContent, IonButtons, IonBackButton
     },
-    computed:{
-        registrationDisabled(){
+    computed: {
+        registrationDisabled() {
             return this.form.username !== '' && this.form.phone !== '' && this.form.password !== '' && this.form.passwordConfirmation !== '';
         },
         ...mapGetters({
 
         })
     },
-    data(){
+    data() {
         return {
-            form:{
-                username: '',
-                phone: '',
-                password: '',
-                passwordConfirmation: ''
-            }
+            form: {
+                username: 'aa',
+                phone: '+966548226392',
+                password: '123456',
+                passwordConfirmation: '123456'
+            },
+            smsSent: true,
+            appVerifier: '',
+            otp: '',
+            confirmationResult: ''
         }
     },
-    methods:{
+    mounted() {
+        this.initRecaptcha();
+    },
+    methods: {
         ...mapActions({
 
-        })
+        }),
+        signIn() {
+            const auth = getAuth();
+            signInWithPhoneNumber(auth, this.form.phone, this.appVerifier)
+                .then((confirmationResult) => {
+                    this.confirmationResult = confirmationResult;
+                    // console.log(JSON.stringify(confirmationResult))
+                    // SMS sent. Prompt user to type the code from the message, then sign the
+                    // user in with confirmationResult.confirm(code).
+
+                    // window.confirmationResult = confirmationResult;
+                    this.smsSent = true;
+                    // ..
+                }).catch((error) => {
+                    // Error; SMS not sent
+                    // ...
+                    alert(error);
+                });
+        },
+        confirmOTP() {
+            this.confirmationResult.confirm(this.otp).then(() => {
+                this.$router.push({ name: 'home' })
+            }).catch((err) => {
+                alert(JSON.stringify(err))
+            });
+        },
+        initRecaptcha() {
+            const auth = getAuth();
+            this.appVerifier = new RecaptchaVerifier('sign-in-button', {
+                'size': 'invisible',
+            }, auth);
+        }
     },
-    setup(){
+    setup() {
         const router = useRouter();
         return {
             router
@@ -87,11 +152,11 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-#forgotPasswordButton{
+#forgotPasswordButton {
     --padding-start: 0
 }
 
-#registerButton{
+#registerButton {
     --padding-start: 0;
     --padding-end: 0;
 }
